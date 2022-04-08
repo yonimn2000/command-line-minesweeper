e851edaf4fe95986845f986c3b6d760f7ccb84cd
Yonatan Mankovich <yonimn2000@gmail.com>
2022-04-08 14:17:08 -0400

    Add board option checks.

--- CommandLineMinesweeper.Core/BoardOptions.cs
+++ CommandLineMinesweeper.Core/BoardOptions.cs
@@ -2,10 +2,30 @@
 {
     public class BoardOptions
     {
-        public int Height { get; set; } = 20;
-        public int Width { get; set; } = 20;
-        public double MinesRatio { get; set; } = 0.15;
-        public int? RandomSeed { get; set; } = null;
-        public int GetNumberOfMines() => (int)(Height * Width * MinesRatio);
+        public int Height { get; }
+        public int Width { get; }
+        public double MinesRatio { get; }
+        public int? RandomSeed { get; }
+
+        public BoardOptions() : this(height: 20, width: 20, minesRatio: 0.15) { }
+
+        public BoardOptions(int height, int width, double minesRatio, int? randomSeed = null)
+        {
+            if (height < 1)
+                throw new ArgumentException($"Board height cannot be less than 1.", nameof(height));
+
+            if (width < 1)
+                throw new ArgumentException("Board width cannot be less than 1.", nameof(width));
+
+            if (minesRatio < 0)
+                throw new ArgumentException("Board mines ratio cannot be negative.", nameof(minesRatio));
+
+            Height = height;
+            Width = width;
+            MinesRatio = minesRatio;
+            RandomSeed = randomSeed;
+        }
+
+        public int GetNumberOfMines() => (int)Math.Round(Height * Width * MinesRatio);
     }
 }
